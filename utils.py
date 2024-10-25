cafc5ef10938b447cc2d0e45dccf37696ccaae6a
yadonglu <yadonglu@microsoft.com>
2024-10-25 16:09:24 -0700

    add florence model; update demo.ipynb

--- utils.py
+++ utils.py
@@ -33,19 +33,27 @@ import supervision as sv
 import torchvision.transforms as T
 
 
-def get_caption_model_processor(model_name_or_path="Salesforce/blip2-opt-2.7b", device=None):
+def get_caption_model_processor(model_name, model_name_or_path="Salesforce/blip2-opt-2.7b", device=None):
     if not device:
         device = "cuda" if torch.cuda.is_available() else "cpu"
-    from transformers import Blip2Processor, Blip2ForConditionalGeneration
-    processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
-    if device == 'cpu':
-        model = Blip2ForConditionalGeneration.from_pretrained(
-        model_name_or_path, device_map=None, torch_dtype=torch.float32
-    ) 
-    else:
-        model = Blip2ForConditionalGeneration.from_pretrained(
-        model_name_or_path, device_map=None, torch_dtype=torch.float16
-    )
+    if model_name == "blip2":
+        from transformers import Blip2Processor, Blip2ForConditionalGeneration
+        processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+        if device == 'cpu':
+            model = Blip2ForConditionalGeneration.from_pretrained(
+            model_name_or_path, device_map=None, torch_dtype=torch.float32
+        ) 
+        else:
+            model = Blip2ForConditionalGeneration.from_pretrained(
+            model_name_or_path, device_map=None, torch_dtype=torch.float16
+        ).to(device)
+    elif model_name == "florence2":
+        from transformers import AutoProcessor, AutoModelForCausalLM 
+        processor = AutoProcessor.from_pretrained("microsoft/Florence-2-base", trust_remote_code=True)
+        if device == 'cpu':
+            model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=torch.float32, trust_remote_code=True)
+        else:
+            model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=torch.float16, trust_remote_code=True).to(device)
     return {'model': model.to(device), 'processor': processor}
 
 
