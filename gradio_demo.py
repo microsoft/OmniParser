b43950348ece390cf5d9d49e8c7b20bc1487e803
Richard Abrich <richard.abrich@gmail.com>
2024-10-29 16:41:00 -0400

    add missing json import

--- gradio_demo.py
+++ gradio_demo.py
@@ -1,16 +1,12 @@
 from typing import Optional
-
 import gradio as gr
-import numpy as np
 import torch
 from PIL import Image
 import io
+import base64
+import json
 
-
-import base64, os
 from utils import check_ocr_box, get_yolo_model, get_caption_model_processor, get_som_labeled_img
-import torch
-from PIL import Image
 
 yolo_model = get_yolo_model(model_path='weights/icon_detect/best.pt')
 caption_model_processor = get_caption_model_processor(model_name="florence2", model_name_or_path="weights/icon_caption_florence")
